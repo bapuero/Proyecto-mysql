a169ff0889153837342f4688598d90a7bb34e508
bapuero <69888223+bapuero@users.noreply.github.com>
2020-08-23 18:29:33 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -5,51 +5,39 @@ cur = miConexion.cursor()
 numero = input("Eliga una opcion: ")
 while(numero in ["1", "2", "3", "4"]):
     if numero == "1":
-        cur.execute("ALTER TABLE cliente MODIFY telefono INT DEFAULT NULL;")
         cur.execute("INSERT INTO cliente (IDcliente, Nombre_C, Direccion, Dinero) VALUES(527498160, 'Bruno', 'VillaClub', 50);")
-        cur.execute("SELECT IDcliente, Nombre_C, Direccion, Telefono, Dinero FROM cliente;")
-        for IDcliente, Nombre_C, Direccion, Telefono, Dinero in cur.fetchall():
-            print(IDcliente, Nombre_C, Direccion, Telefono, Dinero)
-        cur.execute("DELETE FROM cliente WHERE IDcliente = 527498160;")
-        cur.execute("SELECT IDcliente, Nombre_C, Direccion, Telefono, Dinero FROM cliente;")
+        cur.execute("SELECT Nombre_C FROM cliente WHERE Direccion = 'VillaClub';")
+        for Nombre_C in cur.fetchall():
+            print(Nombre_C)
+        cur.execute("UPDATE cliente SET Direccion = 'LaJoya' WHERE IDcliente = 527498160;")
+        cur.execute("SELECT Nombre_C FROM cliente WHERE Direccion = 'LaJoya';")
         print()
-        for IDcliente, Nombre_C, Direccion, Telefono, Dinero in cur.fetchall():
-            print(IDcliente, Nombre_C, Direccion, Telefono, Dinero)
+        for Nombre_C in cur.fetchall():
+            print(Nombre_C)
+        cur.execute("DELETE FROM cliente WHERE IDcliente = 527498160;")
         numero = input("Eliga una opcion: ")
     if numero == "2":
-        cur.execute("ALTER TABLE ebanista MODIFY calificacion INT DEFAULT NULL;")
-        cur.execute("INSERT INTO ebanista (IDebanista, Direccion) VALUES(876543212, 'VillaClub');")
-        cur.execute("SELECT IDebanista, calificacion, direccion, bosquejo FROM ebanista;")
-        for IDebanista, calificacion, direccion, bosquejo in cur.fetchall():
-            print(IDebanista, calificacion, direccion, bosquejo)
+        cur.execute("INSERT INTO ebanista (IDebanista, Direccion, Bosquejo) VALUES(876543212, 'VillaClub', null);")
+        cur.execute("UPDATE ebanista SET calificacion = 6 WHERE IDebanista = 876543212;")
+        cur.execute("SELECT direccion FROM ebanista WHERE calificacion = 6;")
+        for direccion in cur.fetchall():
+            print(direccion)
         cur.execute("DELETE FROM ebanista WHERE IDebanista = 876543212;")
-        cur.execute("SELECT IDebanista, calificacion, direccion, bosquejo FROM ebanista;")
-        print()
-        for IDebanista, calificacion, direccion, bosquejo in cur.fetchall():
-            print(IDebanista, calificacion, direccion, bosquejo)
         numero = input("Eliga una opcion: ")
     if numero == "3":
-        cur.execute("ALTER TABLE pedido MODIFY Color_madera VARCHAR(50) DEFAULT 'Cafe';")
         cur.execute("INSERT INTO pedido (IDcliente, Tipo_madera, Objeto) VALUES(098765432, 'Roble', 'Cama');")
-        cur.execute("SELECT IDcliente, Tipo_madera, Color_madera, Objeto, Bosquejo FROM pedido;")
-        for IDcliente, Tipo_madera, Color_madera, Objeto, Bosquejo in cur.fetchall():
-            print(IDcliente, Tipo_madera, Color_madera, Objeto, Bosquejo)
+        cur.execute("UPDATE pedido SET Color_madera = 'Negro' WHERE IDcliente = 098765432 AND Objeto = 'Cama';")
+        cur.execute("SELECT Objeto FROM pedido WHERE Color_madera = 'Negro';")
+        for Objeto in cur.fetchall():
+            print(Objeto)
         cur.execute("DELETE FROM pedido WHERE (IDcliente = 098765432 AND Objeto = 'Cama');")
-        cur.execute("SELECT IDcliente, Tipo_madera, Color_madera, Objeto, Bosquejo FROM pedido;")
-        print()
-        for IDcliente, Tipo_madera, Color_madera, Objeto, Bosquejo in cur.fetchall():
-            print(IDcliente, Tipo_madera, Color_madera, Objeto, Bosquejo)
         numero = input("Eliga una opcion: ")
     if numero == "4":
-        cur.execute("ALTER TABLE factura MODIFY codigo INT AUTO_INCREMENT NOT NULL;")
         cur.execute("INSERT INTO factura (IDcliente, IDebanista, Dinero, Fecha_entrega) VALUES(987654321, 432109877, 25, '2020-08-08');")
-        cur.execute("SELECT codigo, IDcliente, IDebanista, Dinero, Fecha_Entrega FROM factura;")
-        for codigo, IDcliente, IDebanista, Dinero, Fecha_Entrega in cur.fetchall():
-            print(codigo, IDcliente, IDebanista, Dinero, Fecha_Entrega)
-        cur.execute("DELETE FROM factura WHERE codigo = 11;")
-        cur.execute("SELECT codigo, IDcliente, IDebanista, Dinero, Fecha_Entrega FROM factura;")
-        print()
-        for codigo, IDcliente, IDebanista, Dinero, Fecha_Entrega in cur.fetchall():
-            print(codigo, IDcliente, IDebanista, Dinero, Fecha_Entrega)
+        cur.execute("UPDATE factura SET IDebanista = '765432100' WHERE codigo = 12;")
+        cur.execute("SELECT IDebanista, count(*) AS Total FROM factura GROUP BY IDebanista;")
+        for IDebanista, Total in cur.fetchall():
+            print(IDebanista, Total)
+        cur.execute("DELETE FROM factura WHERE codigo = 12;")
         numero = input("Eliga una opcion: ")
 miConexion.close()
